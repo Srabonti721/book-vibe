86994f07672553d7de4854dee2ed4ed57adbfe97
Srabonti721 <somayaaktarshabonti721@gmail.com>
2025-04-20 09:28:33 +0600

    create navbar and footer

---
+++ src/components/Pages/Error/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <img  src="https://internetdevels.com/sites/default/files/public/blog_preview/404_page_cover.jpg" alt="error img" />
+        </div>
+    );
+};
+
+export default Error;
--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,14 @@
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter,RouterProvider } from 'react-router'
+import { RouterProvider } from 'react-router'
+import { router } from './components/Router/Router.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello World</div>,
-  },
-  {
-    path:'app',
-    Component:App ,
-    },
-]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
- <RouterProvider router={router}/>
+   <RouterProvider router={router}/>
   </StrictMode>,
 )
